5b9e6c3bfd6bc393be18f611a2d99dbc97dade66
Samad Saleem <samad.saleem@fusiontech.global>
2024-06-11 22:39:40 +0500

    starter demo added

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container } from "../styledComponents/signup";
+
+const Signup: React.FC = () => {
+  return (
+    <Container>
+      <input></input>
+    </Container>
+  );
+};
+
+export default Signup;
+
+
+//React hook form 
+//formik
+
+// yup
+
+
+
+//email
+//password
+//confirm password
+
+// Button Submit
